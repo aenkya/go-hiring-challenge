4e314149475bdd7f30a1e466bfda2ee9a7fa7660
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2025-09-12 09:47:59 +0300

    Task 2 & 3: Categories

---
+++ sql/001-0-categories.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    code VARCHAR(32) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
